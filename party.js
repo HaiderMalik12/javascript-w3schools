de453b22c87bdafc17b1bad20a9f4f5633503167
=Haider Malik <haidermalik5044@gmail.com>
2016-02-01 17:46:54 +0500

    Add Step 1 Syntax,Output WhereTo

---
+++ party.js
@@ -0,0 +1,10 @@
+function addHandlder(){
+    document.getElementById('demo').innerHTML='Add is Pressed'
+}
+function updateHandlder(){
+    document.getElementById('demo').innerHTML='Update is Pressed';
+}
+
+function deleteHandlder(){
+    document.getElementById('demo').innerHTML='Delete is Pressed';
+}
